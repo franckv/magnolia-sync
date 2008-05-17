cc60470c1cb61a913a6c28d710884dae227feb87
franck <franck.vdl@gmail.com>
2008-05-17 13:54:01 +0200

    bug fix, refactoring

--- db.py
+++ db.py
@@ -1,7 +1,4 @@
 import sqlite3
-import time
-
-from urlparse import urlparse
 
 class SQLConnector():
     def __init__(self, db):
@@ -25,33 +22,50 @@ class SQLConnector():
 	cur.close()
 	return row[0]
 
-    def insert_tag(self, tag):
+    def remove_all(self):
+	self.remove_all_urls()
+	self.remove_all_bookmarks()
+	self.remove_all_tags()
+
+    def remove_all_tags(self):
+	cur = self.con.cursor()
+	cur.execute('delete from moz_bookmarks where parent=4')
+
+    def remove_all_urls(self):
+	cur = self.con.cursor()
+	cur.execute('delete from moz_places where id in (select fk from moz_bookmarks where type=1)')
+
+    def remove_all_bookmarks(self):
+	cur = self.con.cursor()
+	cur.execute('delete from moz_bookmarks where type=1')
+
+    def insert_tag(self, tag, now):
 	cur = self.con.cursor()
 
-	cur.execute('select id from moz_bookmarks where title=? and parent=4', (tag, ))
+	cur.execute('select id from moz_bookmarks where title=? and parent=4',
+		(tag, ))
 	row = cur.fetchone()
 	if row != None:
 	    self.tags[tag] = row[0]
 	    return
 
-	pos = self.get_current_tag_position()
-	date = time.time() * 1000000
-	cur.execute('insert into moz_bookmarks (type, parent, position, title, dateAdded, lastModified) values (?, ?, ?, ?, ?, ?)', ('2', '4', pos, tag, date, date))
+	pos = self.get_current_tag_position() + 1
+	# TODO: remove these magic numbers
+	cur.execute('insert into moz_bookmarks (type, parent, position, title, dateAdded, lastModified) values (?, ?, ?, ?, ?, ?)',
+		('2', '4', pos, tag, now, now))
 
 	pos = self.insert_id()
 
 	self.tags[tag] = pos
 
-    def insert_url(self, url, title):
+    def insert_url(self, url, title, rev_host):
 	cur = self.con.cursor()
 
-	cur.execute('select id from moz_places where url=?', (url, ))
+	cur.execute('select id from moz_places where url=?',
+		(url, ))
 	row = cur.fetchone()
 	if row != None:
 	    return row[0]
-
-	p = urlparse(url)
-	rev_host = p.hostname[::-1] + '.'
 
 	cur.execute('insert into moz_places (url, title, rev_host) values (?, ?, ?)',
 		(url, title, rev_host))
@@ -66,16 +80,29 @@ class SQLConnector():
 	if row != None:
 	    return
 
-	pos = self.get_current_bookmark_position()
+	pos = self.get_current_bookmark_position() + 1
 	cur.execute('insert into moz_bookmarks (type, fk, parent, position, title, dateAdded, lastModified) values (?, ?, ?, ?, ?, ?, ?)',
 		('1', url_id, '5', pos, title, created, updated))
+
+    def insert_bookmark_toolbar(self, url_id, title, created, updated):
+	cur = self.con.cursor()
+
+	cur.execute('select id from moz_bookmarks where fk=? and parent=3', (url_id, ))
+	row = cur.fetchone()
+	if row != None:
+	    return
+
+	pos = self.get_current_bookmark_toolbar_position() + 1
+	cur.execute('insert into moz_bookmarks (type, fk, parent, position, title, dateAdded, lastModified) values (?, ?, ?, ?, ?, ?, ?)',
+		('1', url_id, '3', pos, title, created, updated))
 
     def insert_bookmark_tag(self, tag, url_id, title, created, updated):
 	cur = self.con.cursor()
 
 	tag_id = self.tags[tag]
 
-	cur.execute('select id from moz_bookmarks where fk=? and parent=?', (url_id, tag_id))
+	cur.execute('select id from moz_bookmarks where fk=? and parent=?',
+		(url_id, tag_id))
 	row = cur.fetchone()
 	if row != None:
 	    return
@@ -85,13 +112,14 @@ class SQLConnector():
 	cur.execute('insert into moz_bookmarks (type, fk, parent, position, title, dateAdded, lastModified) values (?, ?, ?, ?, ?, ?, ?)',
 		('1', url_id, tag_id, pos, title, created, updated))
 
+    # TODO: refactor to only 1 method
     def get_current_tag_position(self):
 	cur = self.con.cursor()
 	cur.execute('select max(position) from moz_bookmarks where type=2 and parent=4')
 	row = cur.fetchone()
 	cur.close()
 	if row[0] == None:
-	    return 0
+	    return -1
 	else:
 	    return row[0]
 
@@ -101,7 +129,17 @@ class SQLConnector():
 	row = cur.fetchone()
 	cur.close()
 	if row[0] == None:
-	    return 0
+	    return -1
+	else:
+	    return row[0]
+
+    def get_current_bookmark_toolbar_position(self):
+	cur = self.con.cursor()
+	cur.execute('select max(position) from moz_bookmarks where type=1 and parent=3')
+	row = cur.fetchone()
+	cur.close()
+	if row[0] == None:
+	    return -1
 	else:
 	    return row[0]
 
@@ -110,11 +148,12 @@ class SQLConnector():
 
 	tag_id = self.tags[tag]
 
-	cur.execute('select max(position) from moz_bookmarks where type=1 and parent=?', (tag_id, ))
+	cur.execute('select max(position) from moz_bookmarks where type=1 and parent=?',
+		(tag_id, ))
 	row = cur.fetchone()
 	cur.close()
 	if row[0] == None:
-	    return 0
+	    return -1
 	else:
 	    return row[0]
 
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from db import SQLConnector
 
 import time
 import datetime
+from urlparse import urlparse
 
 key_file = open('key', 'r')
 api_key = key_file.readline().rstrip()
@@ -42,74 +43,34 @@ def main():
     db = SQLConnector('/home/franck/places.sqlite')
     db.connect()
 
+    db.remove_all()
+
+    now = time.time() * 1000000
     tags = pyg.tags_find(myself)
     for tag in tags:
-        db.insert_tag(tag.name.lower())
+        db.insert_tag(tag.name.lower(), now)
 
     time.sleep(1)
 
     bookmarks = pyg.bookmarks_find(person=myself)
     for bookmark in bookmarks:
-	url_id = db.insert_url(bookmark.url, bookmark.title)
+	p = urlparse(bookmark.url)
+	rev_host = p.hostname[::-1] + '.'
+	url_id = db.insert_url(bookmark.url, bookmark.title, rev_host)
 	created = str(get_timestamp(bookmark.created))
 	updated = str(get_timestamp(bookmark.updated))
 	db.insert_bookmark(url_id, bookmark.title, created, updated)
 	tags = bookmark.tags
 	for tag in tags:
 	    db.insert_bookmark_tag(tag.lower(), url_id, bookmark.title, created, updated)
+	    # ok this is just a personal convention
+	    # TODO: make it more generic
+	    if tag == 'toolbar':
+		db.insert_bookmark_toolbar(url_id, bookmark.title, created, updated)
+
    
     db.commit()
     db.close()
-
-
-
-#Tags = pyg.tags_find(myself)
-#
-#For tag in tags:
-#    print tag.name
-#    print tag.count
-#
-#Time.sleep(1)
-
-#bookmarks = pyg.bookmarks_find(person=myself,tags='anime')
-#for bookmark in bookmarks:
-#    print bookmark
-#    created = time.strptime(bookmark.created, '%Y-%m-%dT%H:%M:%S-%Z:00')
-#    print time(created)
-
-
-#b = pyg.Bookmarks_Find(tags='emacs',person='inkedmn')
-#c = pyg.Bookmarks_Update(id='voyenir',tags=['foo','bar','baz']) 
-#d = pyg.Bookmarks_Delete(['vostigiba'])
-#b = pyg.Bookmarks_Add('Test Bookmark','http://somecrap.com',
-#	                      'description!','0',['foo','bar'],3)
-#d = pyg.Bookmarks_Get(['hohogalath','whosestete'])
-#
-#r = pyg.Bookmarks_Tags_Add(['voyenir'],['bling','blong'])
-#d = pyg.Bookmarks_Tags_Delete(['voyenir'],['foo'])
-#
-#p = pyg.Bookmarks_Tags_Replace(['voyenir'],'bar','bling')
-
-
-#
-#    alltags = api.tags_find(person=myself, tags='manga')
-#	for tag in alltags:
-#	    print tag
-#	
-#    bms = api.bookmarks_find(person=myself, tags='manga', limit='5')
-#    print len(bms)
-#    for bm in bms:
-#      print bm.id
-#      print ' ' + bm.title
-#      print ' ' + bm.url
-#      print ' ' + bm.description
-#      print ' ' + bm.created[0:10]
-#      print ' ' + bm.updated[0:10]
-#      for tag  in bm.tags:
-#	print ' >>' + tag
-#      print
-#
-
 
 if __name__ == '__main__':
     main()
